453aae03d88c2cd706821120c8f8d7dcea216ff3
Siming <siming535@gmail.com>
2021-01-18 22:50:11 +0800

    Update index.js

--- pages/show/index.js
+++ pages/show/index.js
@@ -4,7 +4,14 @@ Page({
   data: {
     isEdit: false, // 是否是编辑状态
   },
-  onShow() {
+  onLoad (option) {
+    // 存储邀请码
+    if (option.scene) {
+      const par = decodeURIComponent(option.scene)
+      console.log(par)
+      const ICode = app.getQueryString(par, 'uid')
+      wx.setStorageSync('ICode', ICode)
+    }
   },
   // 去逛逛
   goUrl: app.throttle(function () {
